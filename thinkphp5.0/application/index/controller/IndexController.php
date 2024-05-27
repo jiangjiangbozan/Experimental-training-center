66a6efe76021904890a9160aa418389aa9de6134
chenjunnian <3112037487@qq.com>
2024-05-27 16:35:21 +0800

    首页修改

--- thinkphp5.0/application/index/controller/IndexController.php
+++ thinkphp5.0/application/index/controller/IndexController.php
@@ -3,7 +3,13 @@ namespace app\index\controller;
 use think\Controller;   // 用于与V层进行数据传递
 use app\common\model\News; 
 use app\common\model\Notification; 
-use think\Session;     
+use app\common\model\CarouselImage;
+use app\common\model\Laboratory;
+use app\common\model\Slide;
+use app\common\model\NewsImage;
+use app\common\model\Zi;
+use think\db;
+use think\Session;          
 
 class IndexController extends Controller
 {
@@ -11,12 +17,43 @@ class IndexController extends Controller
     {
         $News = new News; 
         $Notification = new Notification; 
-        $notifications = Notification::paginate(5);
+        $notifications = Notification::paginate(10);
         $new = News::paginate(10);
     	$power = Session::get('power');
     	$this->assign('power',$power);
         $this->assign('notifications', $notifications);
         $this->assign('new', $new);
+
+        // 查询轮播图数据
+        $carouselImages = CarouselImage::all();
+        
+        // 分配数据到视图
+        $this->assign('carouselImages',$carouselImages);
+
+         // 查询实验室信息
+        $laboratory = Laboratory::find(1); // 假设您要查询的是 ID 为 1 的实验室
+        
+        // 分配数据到视图
+        $this->assign('laboratory',$laboratory);
+
+        // 查询幻灯片信息
+        $slides = Slide::all();
+        
+        // 分配数据到视图
+        $this->assign('slides',$slides);
+
+        // 查询新闻轮播图信息
+        $newsImages = NewsImage::all();
+        
+        // 分配数据到视图
+        $this->assign('newsImages',$newsImages);
+
+        // 查询新闻轮播图信息
+        $zis = Zi::all();
+        
+        // 分配数据到视图
+        $this->assign('zis',$zis);
+
         $htmls = $this->fetch(); // 取回打包后的数据
         return $htmls; // 将数据返回给用户
         
